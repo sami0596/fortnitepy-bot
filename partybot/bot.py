2b3f9fa327529c61df01b9a908291e3b342326c6
sami0596 <gamesteamdk@hotmail.com>
2021-04-29 01:31:41 +0200

    Update bot.py

--- partybot/bot.py
+++ partybot/bot.py
@@ -104,7 +104,7 @@ class PartyBot(commands.Bot):
                 details=f"Logged in as {self.user.display_name}.",
                 state=f"{self.party.leader.display_name}'s party.",
                 large_image="skull_trooper",
-                large_text="discord.gg/fnpy",
+                large_text="HelloHi",
                 small_image="outfit",
                 small_text=outfit,
                 start=int(start_time),
